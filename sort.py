f6251db297c9b702d2a4ac93e43b5dedcfe149ed
ZHmao <ZHmao@users.noreply.github.com>
2015-08-19 22:14:15 +0800

    add quick sort

--- sort.py
+++ sort.py
@@ -101,3 +101,39 @@ def merge_sort(a_list):
             a_list[k] = right_half[j]
             j += 1
             k += 1
+
+
+# quick sort
+
+def quick_sort(a_list):
+    quick_sort_helper(a_list, 0, len(a_list)-1)
+
+def quick_sort_helper(a_list, start, end):
+    if start < end:
+        split_position = partition(a_list, start, end)
+
+        quick_sort_helper(a_list, start, split_position-1)
+        quick_sort_helper(a_list, split_position+1, end)
+
+def partition(a_list, start, end):
+    pivot_value = a_list[start]
+    left_mark = start+1
+    right_mark = end
+    done = False
+    
+    while not done:
+        
+        while left_mark <= right_mark and a_list[left_mark] <= pivot_value:
+            left_mark += 1
+            
+        while right_mark >= left_mark and a_list[right_mark] >= pivot_value:
+            right_mark -= 1
+            
+        if right_mark < left_mark:
+            done = True
+        else:
+            a_list[left_mark], a_list[right_mark] = a_list[right_mark], a_list[left_mark]
+            
+    a_list[start], a_list[right_mark] = a_list[right_mark], a_list[start]
+    
+    return right_mark
